d50e1d44a8576ab968d0720cf6dbccf26f6b0d3b
aqilmuyasar <44615102+aqilmuyasar@users.noreply.github.com>
2020-09-11 23:20:14 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggle() {
+  let a = document.getElementById("navbar");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
